fa9563e462eb1e0b897322bfaccd5dd5344710bb
Nygel Porterfield <nygel.porterfield@gmail.com>
2019-09-04 01:47:31 -0400

    Updates

---
+++ precache-manifest.b1bf9cf64a8c55e752e1a9effc9a480e.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bdedb7c5bbac738bd27fe5591bf59b",
+    "url": "/urfavoriteauthorSite/index.html"
+  },
+  {
+    "revision": "e8ff97b4d20ed37ec959",
+    "url": "/urfavoriteauthorSite/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "116ca11f085c32a3dd47",
+    "url": "/urfavoriteauthorSite/static/css/main.29fdbe0b.chunk.css"
+  },
+  {
+    "revision": "e8ff97b4d20ed37ec959",
+    "url": "/urfavoriteauthorSite/static/js/2.03234bc5.chunk.js"
+  },
+  {
+    "revision": "116ca11f085c32a3dd47",
+    "url": "/urfavoriteauthorSite/static/js/main.8fd36e15.chunk.js"
+  },
+  {
+    "revision": "d5fdc95b12045cfc9706",
+    "url": "/urfavoriteauthorSite/static/js/runtime~main.9a37506b.js"
+  },
+  {
+    "revision": "a47d74e244ab5d652e891828efd86ebc",
+    "url": "/urfavoriteauthorSite/static/media/BARON.a47d74e2.JPG"
+  },
+  {
+    "revision": "6fe4070bf3a8110c4a0c02db4e8d49a6",
+    "url": "/urfavoriteauthorSite/static/media/algorithm.6fe4070b.JPG"
+  },
+  {
+    "revision": "e7a8b26c3f06b0fb8c7a727b0f38c90f",
+    "url": "/urfavoriteauthorSite/static/media/bookcollage.e7a8b26c.jpg"
+  },
+  {
+    "revision": "85855daa28cd017f9c821f8b762909a4",
+    "url": "/urfavoriteauthorSite/static/media/bulletproofheart.85855daa.jpg"
+  },
+  {
+    "revision": "67b300e215e8eca38975bdea04b3abb6",
+    "url": "/urfavoriteauthorSite/static/media/clairvoyance.67b300e2.jpg"
+  },
+  {
+    "revision": "1a3f505ae76c3923d51da9241233f137",
+    "url": "/urfavoriteauthorSite/static/media/daughterofthelastdays.1a3f505a.JPG"
+  },
+  {
+    "revision": "b188c211b67b40c0cd56a6d1f207d741",
+    "url": "/urfavoriteauthorSite/static/media/deadinthewater.b188c211.JPG"
+  },
+  {
+    "revision": "528483aac28b5806c7a16b693fb30a98",
+    "url": "/urfavoriteauthorSite/static/media/deadmanwalking.528483aa.JPG"
+  },
+  {
+    "revision": "39a1d68aec01e3a9ce7c0ad7b9a9c6b2",
+    "url": "/urfavoriteauthorSite/static/media/facebook.39a1d68a.png"
+  },
+  {
+    "revision": "b9bd4e96a784be8e052eb1c8179da48e",
+    "url": "/urfavoriteauthorSite/static/media/ghost.b9bd4e96.PNG"
+  },
+  {
+    "revision": "72096ae7e018b2c6091f57e3ceacf055",
+    "url": "/urfavoriteauthorSite/static/media/golem.72096ae7.jpg"
+  },
+  {
+    "revision": "20df27d8061e9d44431e3ca6d827c9f7",
+    "url": "/urfavoriteauthorSite/static/media/herocity.20df27d8.jpg"
+  },
+  {
+    "revision": "8a6c2f1057587ce587240f8226c7e522",
+    "url": "/urfavoriteauthorSite/static/media/hubris.8a6c2f10.PNG"
+  },
+  {
+    "revision": "03f40b67b63d9c1c1a5a792109bbc699",
+    "url": "/urfavoriteauthorSite/static/media/instagram.03f40b67.png"
+  },
+  {
+    "revision": "65771b329328ea9218d703f4cacf1084",
+    "url": "/urfavoriteauthorSite/static/media/journee3.65771b32.JPG"
+  },
+  {
+    "revision": "78b9b50a8cdd65dcd133601c36e344c9",
+    "url": "/urfavoriteauthorSite/static/media/journeythetraveler.78b9b50a.JPG"
+  },
+  {
+    "revision": "8106f74d8811b67b5abef59dee936c34",
+    "url": "/urfavoriteauthorSite/static/media/journeythetravelerBEYOND.8106f74d.JPG"
+  },
+  {
+    "revision": "604d120b18f1e43596d794d00b12bced",
+    "url": "/urfavoriteauthorSite/static/media/levels.604d120b.JPG"
+  },
+  {
+    "revision": "538df2db3988859ca7eb1a8dfae68a6b",
+    "url": "/urfavoriteauthorSite/static/media/missingpiece.538df2db.JPG"
+  },
+  {
+    "revision": "7d6b8c3e1cbfdd69c28f7e2df34a11e6",
+    "url": "/urfavoriteauthorSite/static/media/moonbreaker.7d6b8c3e.PNG"
+  },
+  {
+    "revision": "c7fe346a1949415ca187a0913afea3ba",
+    "url": "/urfavoriteauthorSite/static/media/patreon.c7fe346a.png"
+  },
+  {
+    "revision": "fadf6dde5eea63f5265166546154f453",
+    "url": "/urfavoriteauthorSite/static/media/phantasy.fadf6dde.JPG"
+  },
+  {
+    "revision": "32cf45364b4e84dd0c99f8f33fe6a6d5",
+    "url": "/urfavoriteauthorSite/static/media/prom.32cf4536.JPG"
+  },
+  {
+    "revision": "bc84c588fe976f0054067e1c8de9fb67",
+    "url": "/urfavoriteauthorSite/static/media/realmofthefallen.bc84c588.JPG"
+  },
+  {
+    "revision": "f3c4c517ad240be566435444d285fabe",
+    "url": "/urfavoriteauthorSite/static/media/remmanifest.f3c4c517.JPG"
+  },
+  {
+    "revision": "82936edf1f80ecdaea2780852c68c657",
+    "url": "/urfavoriteauthorSite/static/media/spellbound.82936edf.JPG"
+  },
+  {
+    "revision": "588891299d20c12af2d98ccbcb1a6db7",
+    "url": "/urfavoriteauthorSite/static/media/storytime.58889129.JPG"
+  },
+  {
+    "revision": "274abf5e7d126d4b36b2d3bd79e20378",
+    "url": "/urfavoriteauthorSite/static/media/thecards.274abf5e.JPG"
+  },
+  {
+    "revision": "246087bd4f1f438092ba0ae500ac9cc0",
+    "url": "/urfavoriteauthorSite/static/media/theloversmanual.246087bd.JPG"
+  },
+  {
+    "revision": "827e6dcf5592069f689a8419c7e92938",
+    "url": "/urfavoriteauthorSite/static/media/thrillghost.827e6dcf.JPG"
+  },
+  {
+    "revision": "db55e92ace31396ce17d884377b7c301",
+    "url": "/urfavoriteauthorSite/static/media/thrillhunted.db55e92a.JPG"
+  },
+  {
+    "revision": "39d57dfb8f1258222d11475bb8cd4a8a",
+    "url": "/urfavoriteauthorSite/static/media/thrilllost.39d57dfb.PNG"
+  },
+  {
+    "revision": "3927a1639f0b52c563aa0c43d9ad6086",
+    "url": "/urfavoriteauthorSite/static/media/timebreakers.3927a163.JPG"
+  },
+  {
+    "revision": "98d43cba5f894a0c1feb12f8b94a5da2",
+    "url": "/urfavoriteauthorSite/static/media/twitter.98d43cba.png"
+  },
+  {
+    "revision": "3bc51f0da2ce98eb52ea6b3adea1211a",
+    "url": "/urfavoriteauthorSite/static/media/urfavoriteauthor.3bc51f0d.JPG"
+  },
+  {
+    "revision": "76997b2347c49d1edf97036b4f82478c",
+    "url": "/urfavoriteauthorSite/static/media/warlok.76997b23.JPG"
+  },
+  {
+    "revision": "449a589b2356344692ed91f0b972a2f2",
+    "url": "/urfavoriteauthorSite/static/media/wildlyfe.449a589b.JPG"
+  },
+  {
+    "revision": "fd942e64c1b41ea64a8019b2f0bda527",
+    "url": "/urfavoriteauthorSite/static/media/zombiesdontcome.fd942e64.PNG"
+  }
+]);
